608efdf894dbcf10e853ea3b2211ad9b53425ff1
Misha Petrov <modesau@gmail.com>
2018-04-06 18:42:00 -0700

    output user polls component on private component

--- src/app/private/private.component.ts
+++ src/app/private/private.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NgModule } from '@angular/core'
 import * as firebase from "firebase";
 import { AuthenticationService } from '../authentication.service';
 
+@NgModule({
+  imports: [MatTabsModule]
+})
 @Component({
   selector: 'app-private',
   templateUrl: './private.component.html',
