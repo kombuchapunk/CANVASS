26e99e19a874e710e4be0440e6dd145ee0d185c0
Misha Petrov <modesau@gmail.com>
2018-04-07 15:55:57 -0700

    rename public to login

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PublicComponent } from './public.component';
+import { LoginComponent } from './login.component';
 
-describe('PublicComponent', () => {
-  let component: PublicComponent;
-  let fixture: ComponentFixture<PublicComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PublicComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PublicComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-public',
-  templateUrl: './public.component.html',
-  styleUrls: ['./public.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
-export class PublicComponent {
+export class LoginComponent {
   private user;
 
   constructor() {}
