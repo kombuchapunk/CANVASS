55e4d73d56e01920bbeb67524e3e0f90252e8d90
Misha Petrov <modesau@gmail.com>
2018-04-06 20:53:30 -0700

    write function to get a random hex formatted gradient for html

--- src/app/user-polls/user-polls.component.ts
+++ src/app/user-polls/user-polls.component.ts
@@ -9,19 +9,29 @@ import * as firebase from "firebase";
 @Component({
   selector: 'app-user-polls',
   templateUrl: './user-polls.component.html',
-  styleUrls: ['./user-polls.component.css'],
+  styleUrls: ['./user-polls.component.scss'],
   providers: [PollService, AuthenticationService]
 })
-export class UserPollsComponent  {
+export class UserPollsComponent implements OnInit {
   private user;
   polls: FirebaseListObservable<any[]>;
   userId: string;
+  gradientList=["#ff758c", "#50c9c3", "#ffc3a0", "#614385", "#43cea2", "#f4e2d8", "#ffd89b", "#19547b", "#c4e0e5", "#ffedbc"];
 
   constructor(private pollService: PollService, private authService: AuthenticationService) {
   }
 
+  ngOnInit() {
+    this.polls = this.pollService.getAllPolls();
+    console.log(this.randomGradient());
+  }
+
   ngDoCheck() {
     this.user = firebase.auth().currentUser;
+  }
+
+  randomGradient() {
+    return (this.gradientList[Math.floor(Math.random() * 10)] + ", " + this.gradientList[Math.floor(Math.random() * 10)]);
   }
 
   // getUserPolls() {
