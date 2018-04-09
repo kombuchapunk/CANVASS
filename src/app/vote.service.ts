8d3509329218f0e6be8ae428bbe787a7eec2eff0
Misha Petrov <modesau@gmail.com>
2018-04-08 17:39:43 -0700

    add functions to create or update a vote

--- src/app/vote.service.ts
+++ src/app/vote.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Injectable()
 export class VoteService {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  getChoice1Votes(itemId): FirebaseObjectObservable<any> {
+    // Gets total votes
+    return this.db.object(`choice1votes/${itemId}`)
+  }
+
+  getChoice2Votes(itemId): FirebaseObjectObservable<any> {
+    // Gets total votes
+    return this.db.object(`choice2votes/${itemId}`)
+  }
+
+  updateUserChoice1Vote(itemId, userId, vote): void {
+    // Creates or updates user's vote
+    let data = {}
+    data[userId] = vote
+    this.db.object(`choice1vote/${itemId}`).update(data)
+  }
 
 }
